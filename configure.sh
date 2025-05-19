ff96179fff3d88ac2f84719911f8bbecf11edc67
Jeffrey Qian <38968278+jeffreyqdd@users.noreply.github.com>
2025-05-18 21:59:59 -0400

    add install prefix (#19)
    * add install prefix
    
    * add instructions to readme

--- configure.sh
+++ configure.sh
@@ -7,6 +7,7 @@ BUILD_TYPE="Debug"
 BUILD_DIR="build"
 BUILD_EXAMPLES=OFF
 BUILD_TESTS=OFF
+PYTHON_UDL_INTERFACE_PREFIX="${PYTHON_UDL_INTERFACE_PREFIX:-/usr/local}"
 
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
@@ -25,12 +26,17 @@ while [[ "$#" -gt 0 ]]; do
     -t | --tests)
         BUILD_TESTS=ON
         ;;
+    -p | --prefix)
+        PYTHON_UDL_INTERFACE_PREFIX="$2"
+        shift
+        ;;
     -h | --help)
-        echo "Usage: ./configure.sh [-t Debug|Release|RelWithDebInfo|MinSizeRel] [-d build_dir] [-e]"
+        echo "Usage: ./configure.sh [options]"
         echo "  -m | --mode      : Build type (default: Debug)"
         echo "  -d | --dir       : Build directory (default: build)"
         echo "  -e | --examples  : Enable building examples (default: OFF)"
         echo "  -t | --tests     : Enable building tests (default: OFF)"
+        echo "  -p | --prefix    : Install prefix (default: /usr/local or \$PYTHON_UDL_INTERFACE_PREFIX)"
         exit 0
         ;;
     *)
@@ -46,6 +52,7 @@ echo "  Build type     : $BUILD_TYPE"
 echo "  Build dir      : $BUILD_DIR"
 echo "  Build examples : $BUILD_EXAMPLES"
 echo "  Build tests    : $BUILD_TESTS"
+echo "  Install prefix : $PYTHON_UDL_INTERFACE_PREFIX"
 
 # Create build directory
 mkdir -p "$BUILD_DIR"
@@ -53,6 +60,7 @@ mkdir -p "$BUILD_DIR"
 # Run CMake configuration
 cmake -B "$BUILD_DIR" \
     -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+    -DCMAKE_INSTALL_PREFIX="$PYTHON_UDL_INTERFACE_PREFIX" \
     -DBUILD_EXAMPLES="$BUILD_EXAMPLES" \
     -DBUILD_TESTS="$BUILD_TESTS" \
     -S .
