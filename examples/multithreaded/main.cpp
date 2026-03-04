b0f1004fd73086ea2ad9b3537a4d93ba5f007f30
Jeffrey Qian <jeffreyqdd@gmail.com>
2026-03-03 21:32:39 -0600

    multithreaded example

---
+++ examples/multithreaded/main.cpp
@@ -0,0 +1,75 @@
+#include <pyscheduler/pyscheduler.hpp>
+
+#include <chrono>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+using namespace std;
+using namespace pyscheduler;
+
+int main() {
+	constexpr int NUM_REQUESTS = 50;
+	constexpr double SLEEP_SECONDS = 0.01;
+	constexpr int BATCH_SIZE = 8;
+	constexpr int PREFETCH_DEPTH = 2;
+
+	PyManager manager;
+
+	auto module_a = manager.loadPythonModule(
+		"examples.multithreaded.python_modules.module_a", "invoke", BATCH_SIZE, PREFETCH_DEPTH);
+	auto module_b = manager.loadPythonModule(
+		"examples.multithreaded.python_modules.module_b", "invoke", BATCH_SIZE, PREFETCH_DEPTH);
+
+	auto commit = [](double seconds) -> pybind11::object { return pybind11::cast(seconds); };
+	auto callback = [](const pybind11::object& obj) { return obj.cast<double>(); };
+
+	vector<future<double>> futures_a;
+	vector<future<double>> futures_b;
+
+	auto start = chrono::steady_clock::now();
+
+	// Spawn two threads, each queueing work to a different handler
+	thread t1([&] {
+		for(int i = 0; i < NUM_REQUESTS; i++)
+			futures_a.push_back(module_a.queue_invoke(commit, callback, SLEEP_SECONDS));
+	});
+
+	thread t2([&] {
+		for(int i = 0; i < NUM_REQUESTS; i++)
+			futures_b.push_back(module_b.queue_invoke(commit, callback, SLEEP_SECONDS));
+	});
+
+	t1.join();
+	t2.join();
+
+	// Collect results
+	vector<double> results_a;
+	vector<double> results_b;
+
+	for(auto& f : futures_a)
+		results_a.push_back(f.get());
+	for(auto& f : futures_b)
+		results_b.push_back(f.get());
+
+	auto end = chrono::steady_clock::now();
+	auto ms = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+	double expected_sequential = NUM_REQUESTS * 2 * SLEEP_SECONDS * 1000;
+	cout << format("Expected sequential: {:.0f} ms\n", expected_sequential);
+	cout << format("Actual:              {} ms\n", ms);
+	cout << format("Speedup:             {:.2f}x\n", expected_sequential / ms);
+
+	// Write results to file for diffing (outside timed region)
+	ofstream out("/tmp/multithreaded_cpp.txt");
+	for(size_t i = 0; i < results_a.size(); i++)
+		out << format("a[{}] = {:.10g}\n", i, results_a[i]);
+	for(size_t i = 0; i < results_b.size(); i++)
+		out << format("b[{}] = {:.10g}\n", i, results_b[i]);
+
+	cout << "Results written to /tmp/multithreaded_cpp.txt\n";
+
+	return 0;
+}
